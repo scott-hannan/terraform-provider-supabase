5bfc5966c251ddde23ce12c4ba73c60a343bd938
Scott Hannan <117687334+scott-hannan@users.noreply.github.com>
2026-05-15 13:15:41 -0700

    fix: Update apikey_resource.go to force naming requirements

--- internal/provider/apikey_resource.go
+++ internal/provider/apikey_resource.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/google/uuid"
@@ -18,6 +19,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/oapi-codegen/nullable"
@@ -93,6 +95,12 @@ func (d *APIKeyResource) Schema(ctx context.Context, req resource.SchemaRequest,
 			"name": schema.StringAttribute{
 				MarkdownDescription: "Name of the API key",
 				Required:            true,
+				Validators: []validator.String{
+					stringvalidator.RegexMatches(
+						regexp.MustCompile(`^[a-z_][a-z0-9_]*$`),
+						"Name must start with a lowercase letter or an underscore, followed only by lowercase alphanumeric characters or underscore",
+					),
+				},
 			},
 			"description": schema.StringAttribute{
 				MarkdownDescription: "Description of the API key",
